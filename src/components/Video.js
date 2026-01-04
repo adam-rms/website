c98b33dacbc12e0a38740d862c3345c5664d3833
James Bithell <Jbithell@users.noreply.github.com>
2026-01-04 12:10:14 +0000

    Get youtube video working (#80)

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,16 @@ export default function Video() {
   return (
     <section className={styles.video}>
       <div className="text--center  padding-horiz--md">
-        <iframe width="840" height="472" src="https://www.youtube-nocookie.com/embed/iBvYVlspz3E" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe
+          width="840"
+          height="472"
+          src="https://www.youtube-nocookie.com/embed/iBvYVlspz3E?si=dlyASx9xPjkQgWiM"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
       </div>
     </section>
   );
