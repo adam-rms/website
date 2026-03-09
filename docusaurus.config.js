8752f603f365c9c7910699d760c0ab8771180d9c
James Bithell <Jbithell@users.noreply.github.com>
2026-03-09 10:45:30 +0000

    Migrate to Docusaurus v3.9 (#82)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,10 @@
 /* eslint-disable max-len */
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+import {themes as prismThemes} from "prism-react-renderer";
 
 const production = process.env.CONTEXT === "production"; // Netlify/Cloudflare Pages set environment variable "CONTEXT" to "production"/"deploy-preview"
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: "AdamRMS",
   tagline:
     "AdamRMS is a free, open source advanced Rental Management System for Theatre, AV & Broadcast",
@@ -13,105 +12,112 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "warn",
   noIndex: !production,
-  onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
   organizationName: "adam-rms",
   projectName: "adam-rms",
   plugins: ["@docusaurus/plugin-ideal-image"],
-  themeConfig: {
-    navbar: {
-      title: "AdamRMS",
-      logo: {
-        alt: "The AdamRMS Logo",
-        src: "img/logoicon.svg",
-        srcDark: "img/logoicon-white.svg",
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "AdamRMS",
+        logo: {
+          alt: "The AdamRMS Logo",
+          src: "img/logoicon.svg",
+          srcDark: "img/logoicon-white.svg",
+        },
+        items: [
+          {
+            label: "Pricing",
+            to: "/pricing",
+          },
+          {
+            type: "doc",
+            docId: "user-guide/intro",
+            position: "left",
+            label: "User Guide",
+          },
+          {
+            label: "Support",
+            to: "/support",
+          },
+          {
+            type: "doc",
+            docId: "hosting/intro",
+            position: "left",
+            label: "Self Hosting",
+          },
+          {
+            type: "doc",
+            docId: "contributor/intro",
+            position: "left",
+            label: "Contributing",
+          },
+          {
+            href: "https://api-docs.adam-rms.com/",
+            label: "API Documentation",
+          },
+          {
+            label: "Environment",
+            to: "/environment",
+          },
+          {
+            href: "https://dash.adam-rms.com",
+            label: "Login",
+            position: "right",
+          },
+        ],
       },
-      items: [
-        {
-          label: "Pricing",
-          to: "/pricing",
-        },
-        {
-          type: "doc",
-          docId: "user-guide/intro",
-          position: "left",
-          label: "User Guide",
-        },
-        {
-          label: "Support",
-          to: "/support",
-        },
-        {
-          type: "doc",
-          docId: "hosting/intro",
-          position: "left",
-          label: "Self Hosting",
-        },
-        {
-          type: "doc",
-          docId: "contributor/intro",
-          position: "left",
-          label: "Contributing",
-        },
-        {
-          href: "https://api-docs.adam-rms.com/",
-          label: "API Documentation",
-        },
-        {
-          label: "Environment",
-          to: "/environment",
-        },
-        {
-          href: "https://dash.adam-rms.com",
-          label: "Login",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      copyright: `Copyright © 2019-${new Date().getFullYear()} Bithell Studios Ltd. <a href="/legal">Terms</a>`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-    image: "img/banner.jpg",
-    ...(!production && {
-      announcementBar: {
-        id: "dev_build", // Any value that will identify this message.
-        content:
-          'This is a draft version of our website, to view the current version please visit <a href="https://adam-rms.com/">adam-rms.com</a>',
-        backgroundColor: "#fafbfc", // Defaults to `#fff`.
-        textColor: "#091E42", // Defaults to `#000`.
-        isCloseable: false,
+      footer: {
+        style: "dark",
+        copyright: `Copyright © 2019-${new Date().getFullYear()} Bithell Studios Ltd. <a href="/legal">Terms</a>`,
       },
-    }),
-    colorMode: {
-      defaultMode: "light",
-      disableSwitch: false,
-      // using user system preferences, instead of the hardcoded defaultMode
-      respectPrefersColorScheme: true,
-    },
-    ...(process.env.AGOLIA_API_KEY &&
-      process.env.AGOLIA_INDEX &&
-      process.env.AGOLIA_APP_ID && {
-        algolia: {
-          appId: process.env.AGOLIA_APP_ID,
-          apiKey: process.env.AGOLIA_API_KEY,
-          indexName: process.env.AGOLIA_INDEX,
-          contextualSearch: true,
-          searchParameters: {},
-          disableUserPersonalization: true,
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+      image: "img/banner.jpg",
+      ...(!production && {
+        announcementBar: {
+          id: "dev_build", // Any value that will identify this message.
+          content:
+            'This is a draft version of our website, to view the current version please visit <a href="https://adam-rms.com/">adam-rms.com</a>',
+          backgroundColor: "#fafbfc", // Defaults to `#fff`.
+          textColor: "#091E42", // Defaults to `#000`.
+          isCloseable: false,
         },
       }),
-  },
+      colorMode: {
+        defaultMode: "light",
+        disableSwitch: false,
+        // using user system preferences, instead of the hardcoded defaultMode
+        respectPrefersColorScheme: true,
+      },
+      ...(process.env.AGOLIA_API_KEY &&
+        process.env.AGOLIA_INDEX &&
+        process.env.AGOLIA_APP_ID && {
+          algolia: {
+            appId: process.env.AGOLIA_APP_ID,
+            apiKey: process.env.AGOLIA_API_KEY,
+            indexName: process.env.AGOLIA_INDEX,
+            contextualSearch: true,
+            searchParameters: {},
+            disableUserPersonalization: true,
+          },
+        }),
+    }),
   presets: [
     [
       "@docusaurus/preset-classic",
-      {
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: "./sidebars.js",
           editUrl: "https://github.com/adam-rms/website/edit/main/",
           editCurrentVersion: true,
           // includeCurrentVersion: !production,
@@ -128,9 +134,11 @@ module.exports = {
           showReadingTime: true,
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: "./src/css/custom.css",
         },
-      },
+      }),
     ],
   ],
 };
+
+export default config;
